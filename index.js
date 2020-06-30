f527c9c6847aed8908421be7ce0a49ad549ed9b0
RChuaChiaco <ronnster.pokemaster@gmail.com>
2020-06-30 21:22:15 +0800

    added localstorage

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const handlebars = require('handlebars');
 const bodyParser = require('body-parser');
-
 const mongodb = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
+
 
 //creates express app
 const app = express();
@@ -220,14 +220,14 @@ app.post("/addScreening", function(req, res) {
 
 app.post('/getSeatStatus', function(req, res) {
   var query = {
-    slot:ObjectId(req.body.slot) 
+    slot:ObjectId(req.body.slot)
   };
 
     mongoClient.connect(databaseURL, options, function(err, client) {
       if(err) throw err;
 
       const dbo = client.db(dbname);
-  
+
       dbo.collection("seats").find(query).toArray(function(err, result) {
         if(err) throw err;
 
@@ -255,7 +255,13 @@ app.post("/reserveSeats", function(req, res) {
 
 /************Ken Displays */
 app.get("/seatSelection", function(req, res) {
-  var currSlotId = localStorage.getItem("selectedSlot")
+    if (typeof localStorage === "undefined" || localStorage === null) {
+      var LocalStorage = require('node-localstorage').LocalStorage;
+      localStorage = new LocalStorage('./scratch');
+      localStorage.setItem("selectedSlot", "5ec0c846ca85a61340446897");//hardcoded slot id
+      var currSlotId = localStorage.getItem("selectedSlot");
+      console.log("ID:" + localStorage.getItem("selectedSlot"));
+    }
 
     mongoClient.connect(databaseURL, options, function(err, client) {
       if(err) throw err;
@@ -434,7 +440,7 @@ app.get('/movies', function(req, res) {
             res.render('movies', {
               user: user,
               pageCSS: "BigBrain_Screenings",
-              pageJS: "",
+              pageJS: "BigBrain_Screenings",
               pageTitle: "Movie Screenings",
               header: "header",
               footer: "footer",
